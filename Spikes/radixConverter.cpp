55c6272b762ba1155822325b98596abdc3172950
fudgyone <thomaa1@unlv.nevada.edu>
2017-05-04 14:23:15 -0700

    Cleaned, removed extraneous error checking

--- Spikes/radixConverter.cpp
+++ Spikes/radixConverter.cpp
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <stdio.h>
 
 int radixConversion(int baseNum, std::vector<int>& digits) {
 // TODO: finish radixConversion method
@@ -34,39 +35,42 @@ void getDigits(int convNum, std::vector<int>& digits) {
   digits.push_back(convNum % 10); // grab a digit and put it in the digit vector
 }
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main(int argc, const char* argv[]) {
     
-    //error checking
-    int errCode = 0;
+    /************************
+     * Error Message Constant
+     */
     
-    // error checking...
-    std::string errString1  = "ERROR: too few arguments.";
-    std::string usageString = "USAGE: ./radixConverter <base 0-n> <number to convert>";
-    if(argc < 3) { // if too few args, we're done.
-        errCode = 1; // errCode of 1 means too few args.
-        goto errLabel;
-    } else { // otherwise get the first argument as the radix
-      int radixBase = std::atoi(argv[1]); // the radix
-      int radixNum = std::atoi(argv[2]); // the number itself
-        std::cout << radixBase << std::endl; // testing
-	std::cout << radixNum << std::endl; // testing
-        std::cout << errCode << std::endl; // testing
-        if(radixBase > 16 || radixBase < 2) {
-            errCode = 2; // errCode of 2 means the radix is out of bounds
-            goto errLabel; // we're done with an error, so go to the end
-        }
+    const std::string ARG_ERROR = "ERROR: too few arguments.";
+    const std::string USE_ERROR = "USAGE: ./radixConverter <base 0-n> <number to convert>";
+    
+    /**************************
+     * Check the Argument Count
+     */
+    
+    if(argc != 3) {
+        
+        // Arguments are less than three, Print the Message
+        std::cout << ARG_ERROR << std::endl;
+        
+    } else {
+        
+        // We got the right amount of arguments
+        short radixBase = std::atoi(argv[1]); // the radix
+        int   radixNum  = std::atoi(argv[2]); // the number itself
+        
+        std::cout << radixBase << std::endl ; // Print the base
+        std::cout << radixNum  << std::endl ; // Print the number
+        
+        /**********************
+         * Check the Radix Base
+         */
+        
+        if(radixBase > 16 || radixBase < 2) { std::cout << USE_ERROR << std::endl;}
+        
     }
-    // pass arguments to conversion method
     
-    return 0; // and we're done!
     
-    // error checking
-errLabel:
-    if(errCode > 0) { // errCode of anything other than 0 means something went wrong
-// TODO: add errCode functionality (i.e. 1 = too few args, 2 = radix oob, etc)
-        // std::cout << errString1  << std::endl;
-        std::cout << usageString << std::endl;
-    }
-    return 0;
+    return EXIT_SUCCESS; // Return EXIT_SUCCESS (0)
+
 }
