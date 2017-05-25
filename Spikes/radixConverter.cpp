eed7469364c7cb3745c80dee39a160fff89a1f32
fudgyone <thomaa1@unlv.nevada.edu>
2017-05-25 11:39:49 -0700

    up to base 9 works (base 10 redundant but working)

--- Spikes/radixConverter.cpp
+++ Spikes/radixConverter.cpp
@@ -1,12 +1,11 @@
-/*
- Radix Converter: given a radix-base number, convert to decimal
- 
- Usage: "./radixConverter <base 2-16> <number to convert (within 8 digits)>"
- i.e.:  "./radixConverter 2 0101101" spits out "45"
+/*****************************************************************************
+ * Radix Converter: given a radix-base number, convert to decimal
+ *
+ * Usage: "./radixConverter <base 2-16> <number to convert (within 8 digits)>"
+ * i.e.:  "./radixConverter 2 0101101" spits out "45"
  */
-
-// TODO: finish error checking
-// TODO: finish conversion.
+// TODO: fix getDigits to work for base 11 and up (alphanumeric).
+// TODO: finish recursive conversion (no for loop).
 
 #include <iostream>
 #include <vector>
@@ -14,10 +13,36 @@
 #include <stdio.h>
 
 /******************************************************
- * given base number and vector of digits, convert from
- * n-radix to decimal number and return it
+ * Given base number and vector of digits, convert from
+ * n-radix to decimal number and return it.
  */
-int radixConversion(int baseNum, std::vector<int>& digits) {
+int radixConversion(int baseNum, std::vector<int>* digits) {
+    // Declare variables
+    int result  = 0; // Result variable, initially zero
+    int current = 0; // Current variable, initially zero
+    int mult    = 1; // Multiplier variable, initially one
+
+    /**
+     * Recurse through the vector,
+     * add to total, and return.
+     */
+
+    for (int i = digits->size() - 1; i >= 0; i--) { // starting from MSB and moving down
+
+        current = digits->at(i);  // Grab next digit
+        result += current * mult; // Add converted digit to our result
+        mult   *= baseNum;        // Increment our multiplier
+    }
+
+    // We're done, return what we have
+    return result;
+}
+
+/***************************************************
+ * Recursive form of radixConversion method, for fun
+ */
+
+int radixConvRecursion(short baseNum, std::vector<int>* digits) {
 
     return 0;
 }
@@ -25,7 +50,7 @@ int radixConversion(int baseNum, std::vector<int>& digits) {
 /****************************************************
  * get individual digits and place them into a vector
  */
-void getDigits(int convNum, std::vector<int> &digits) {
+void getDigits(int convNum, std::vector<int>* digits) {
   // check if there's still a number to grab
   if (convNum > 9) {
     
@@ -34,12 +59,13 @@ void getDigits(int convNum, std::vector<int> &digits) {
   }
 
   // grab a digit and push it back into the digit vector
-  digits.push_back(convNum % 10);
+  digits->push_back(convNum % 10);
 }
 
 /************************
  * Main of radixConverter
  */
+
 int main(int argc, const char* argv[]) {
     
     /************************
@@ -61,33 +87,33 @@ int main(int argc, const char* argv[]) {
     } else {
         
         // We got the right amount of arguments
-        short radixBase = std::atoi(argv[1]); // the radix
-        int   radixNum  = std::atoi(argv[2]); // the number itself
+        int radixBase = std::atoi(argv[1]); // the radix
+        int radixNum  = std::atoi(argv[2]); // the number itself
 
         /**********************
-        * Check the Radix Base
-        */
+         * Check the Radix Base
+         */
         
-        if(radixBase > 16 || radixBase < 2) { std::cout << USE_ERROR << std::endl;} else {
-        
-            std::cout << radixBase << std::endl ; // Print the base
-            std::cout << radixNum  << std::endl ; // Print the number
+        if(radixBase > 16 || radixBase < 2) { std::cout << USE_ERROR << std::endl;}
 
-        }
+         /************************************
+          * Create a vector and call getDigits
+          */
 
-        /******************************
-         * Create a vector and getDigits
+        std::vector<int>* numVec = new std::vector<int>(0); // Declare our vector and initialize it as empty
+        getDigits(radixNum, numVec);                        // getDigits function call
+
+        /************************************************
+         * Print details, print radixConversion, and exit
          */
 
-        std::vector<int>    numVec;                                   // Declare our vector but do not initialize it
-        getDigits(radixNum, numVec);                                  // getDigits function call
-        std::cout << radixConversion(radixBase, numVec) << std::endl; // radixConversion function call, and printing the result
+        std::cout << "Base:       " << radixBase << std::endl ; // Print the base
+        std::cout << "Number:     " << radixNum  << std::endl ; // Print the number
+        std::cout << "In base 10: " << radixConversion(radixBase, numVec) << std::endl; // radixConversion function call, and printing the result
 
     
     }
 
-
-    
     return EXIT_SUCCESS; // Return EXIT_SUCCESS (0)
 
 }
