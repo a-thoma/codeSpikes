0046fb19ceeb8725197ae91b9bfe246839ef019b
fudgyone <thomaa1@unlv.nevada.edu>
2017-05-25 21:15:48 -0700

    fixed up to base 16

--- Spikes/radixConverter.cpp
+++ Spikes/radixConverter.cpp
@@ -4,13 +4,12 @@
  * Usage: "./radixConverter <base 2-16> <number to convert (within 8 digits)>"
  * i.e.:  "./radixConverter 2 0101101" spits out "45"
  */
-// TODO: fix getDigits to work for base 11 and up (alphanumeric).
+
 // TODO: finish recursive conversion (no for loop).
+// TODO: expand upper bounds of the number to convert. (Just label it?)
 
 #include <iostream>
 #include <vector>
-#include <cstdlib>
-#include <stdio.h>
 
 /******************************************************
  * Given base number and vector of digits, convert from
@@ -48,18 +47,33 @@ int radixConvRecursion(short baseNum, std::vector<int>* digits) {
 }
 
 /****************************************************
- * get individual digits and place them into a vector
+ * Takes a string, converts each individual character
+ * into an integer value, (i.e. A = 10, B = 11, etc.)
+ * and pushes it into the digit vector.
  */
-void getDigits(int convNum, std::vector<int>* digits) {
-  // check if there's still a number to grab
-  if (convNum > 9) {
-    
-    // recurse through the number if there are no more digits to grab
-    getDigits((convNum / 10), digits);  
-  }
+void getChars(std::string convStr, std::vector<int>* digits) {
 
-  // grab a digit and push it back into the digit vector
-  digits->push_back(convNum % 10);
+    /**********************
+     * Loop through convStr
+     */
+    
+     for( int i = 0; i < convStr.length(); i++) {
+
+    /***********************************
+     * Convert char -> int and push back
+     */
+
+     if(convStr.at(i) <= '9') {
+
+        digits->push_back(convStr.at(i) - '0');     // Put it in the vector
+        } else if(convStr.at(i) <= 'F') {
+
+            digits->push_back(convStr.at(i) - '7'); // Uppercase ASCII values
+            } else {
+
+            digits->push_back(convStr.at(i) - 'W'); // Lowercase ASCII values
+        }
+    }
 }
 
 /************************
@@ -73,7 +87,7 @@ int main(int argc, const char* argv[]) {
      */
     
     const std::string ARG_ERROR = "ERROR: too few arguments.";
-    const std::string USE_ERROR = "USAGE: ./radixConverter <base 0-n> <number to convert>";
+    const std::string USE_ERROR = "USAGE: ./radixConverter <base 2-16> <number to convert (up to 8 digits)>";
     
     /**************************
      * Check the Argument Count
@@ -87,33 +101,32 @@ int main(int argc, const char* argv[]) {
     } else {
         
         // We got the right amount of arguments
-        int radixBase = std::atoi(argv[1]); // the radix
-        int radixNum  = std::atoi(argv[2]); // the number itself
+        int radixBase         = std::atoi(argv[1]); // the radix
+        std::string radixStr  = argv[2];            // the number
 
         /**********************
          * Check the Radix Base
          */
         
-        if(radixBase > 16 || radixBase < 2) { std::cout << USE_ERROR << std::endl;}
+        if(radixBase > 16 || radixBase < 2) { std::cout << USE_ERROR << std::endl;} else {
 
-         /************************************
-          * Create a vector and call getDigits
-          */
+             /************************************
+              * Create a vector and call getDigits
+              */
 
-        std::vector<int>* numVec = new std::vector<int>(0); // Declare our vector and initialize it as empty
-        getDigits(radixNum, numVec);                        // getDigits function call
+            std::vector<int>* numVec = new std::vector<int>(0);
+            getChars(radixStr, numVec);
 
-        /************************************************
-         * Print details, print radixConversion, and exit
-         */
+            /************************************************
+             * Print details, print radixConversion, and exit
+             */
 
-        std::cout << "Base:       " << radixBase << std::endl ; // Print the base
-        std::cout << "Number:     " << radixNum  << std::endl ; // Print the number
-        std::cout << "In base 10: " << radixConversion(radixBase, numVec) << std::endl; // radixConversion function call, and printing the result
+            std::cout << "Base:       " << radixBase << std::endl ; // Print the base
+            std::cout << "Number:     " << radixStr  << std::endl ; // Print the number
+            std::cout << "In base 10: " << radixConversion(radixBase, numVec) << std::endl; // radixConversion function call, and printing the result
+        }
 
-    
     }
 
     return EXIT_SUCCESS; // Return EXIT_SUCCESS (0)
-
 }
