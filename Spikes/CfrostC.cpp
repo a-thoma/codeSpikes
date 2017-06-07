fb3e2e507451a4785aa0b79b29116b9f15d4b474
fudgyone <thomaa1@unlv.nevada.edu>
2017-06-07 16:52:46 -0700

    Simplified initial version of CfrostC, will add more functionality as time progresses

--- Spikes/CfrostC.cpp
+++ Spikes/CfrostC.cpp
@@ -1,63 +1,32 @@
 #include <iostream>
 
-class fractalGrid {
-public:
-
-	/****************
-	* Width functions
-	*/
-	void setWidth(int w);  // Set grid width
-	int  getWidth(     );  // Return grid width
-
-	/*****************
-	* Height functions
-	*/
-	void setHeight(int h); // Set grid height
-	int  getHeight(     ); // Return grid height
-
-	/*****************************
-	* Constructors and Destructors
-	*/
-	 fractalGrid(int gW, int gH);         // Class constructor
-	~fractalGrid(              );         // Class destructor
-
-private:
-
-	int gridW     ; // The grid width
-	int gridH     ; // The grid height
-	int grid[][]; // the grid, a 2-dimensional integer array
-
-};
-
-/****************************
-* Setter and Getter functions
+/**********
+* Variables
 */
-void fractalGrid::setWidth(int w) {
-	this->gridW = w;
+
+int gridW = 256; // The grid width
+int gridH = 256; // The grid height
+
+/**********
+* Functions
+*/
+
+void buildFractal() {
+
 }
 
-int fractalGrid::getWidth() {
-	return this->gridW;
-}
+void finalizeFractal() {
 
-void fractalGrid::setHeight(int h) {
-	this->gridH = h;
-}
-
-int fractalGrid::getHeight() {
-	return this->gridH;
 }
 
 int main(int argc, char const *argv[])
 {
-	/**
-	* Create a new fractalGrid
+	/*************************
+	* Create a new fractal grid (2-dimensional array)
 	*/
 
-
-
-
-
-	/* code */
+	/******************
+	* Build the fractal
+	*/
 	return EXIT_SUCCESS;
 }
